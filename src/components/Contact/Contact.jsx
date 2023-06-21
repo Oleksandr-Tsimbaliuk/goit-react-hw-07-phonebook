bcc124bab9ad335300feb5f17ce5e56894f4a1d6
Oleksandr <115025828+Oleksandr-Tsimbaliuk@users.noreply.github.com>
2023-06-21 22:59:56 +0300

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectError } from 'redux/selectors';
+
 
 function Contact({ name, id, phone, deleteContact }) {
 
